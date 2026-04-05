4688fb112de6b4e92786e809cac72feb7aa61689
arifuddincoder <arifuddincoder@gmail.com>
2026-04-05 13:23:14 +0600

    separate schema and add sendResponse.ts to reduce controller response code

--- src/app/module/specialty/specialty.controller.ts
+++ src/app/module/specialty/specialty.controller.ts
@@ -1,20 +1,24 @@
 import { Request, Response } from "express";
 import { SpecialtyServices } from "./specialty.service";
 import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 
 const createSpecialty = catchAsync(async (req: Request, res: Response) => {
 	const payload = req.body;
 	const result = await SpecialtyServices.createSpecialty(payload);
-	res.status(201).json({
+	sendResponse(res, {
+		httpStatusCode: 201,
 		success: true,
-		message: "Specialty created successfully!",
+		message: "Created specialties successfully",
 		data: result,
 	});
 });
 
 const getAllSpecialty = catchAsync(async (req: Request, res: Response) => {
 	const result = await SpecialtyServices.getAllSpecialty();
-	res.status(200).json({
+
+	sendResponse(res, {
+		httpStatusCode: 200,
 		success: true,
 		message: "Fetched specialties successfully",
 		data: result,
@@ -25,7 +29,8 @@ const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
 	const id = req.params.id;
 	const result = await SpecialtyServices.deleteSpecialty(id as string);
 
-	res.status(200).json({
+	sendResponse(res, {
+		httpStatusCode: 200,
 		success: true,
 		message: "Deleted specialty successfully",
 		data: result,
