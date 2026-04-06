3604491b95fd3efa3710854a7d9616e70ca9e0fa
arifuddincoder <arifuddincoder@gmail.com>
2026-04-06 17:09:14 +0600

    add createDoctor service code

--- src/app/module/user/user.interface.ts
+++ src/app/module/user/user.interface.ts
@@ -1,3 +1,4 @@
+/*
 model Doctor {
   id            String  @id @default(uuid(7))
   name          String
@@ -27,4 +28,26 @@ model Doctor {
   @@index([email], name: "idx_doctor_email")
   @@index([isDeleted], name: "idx_doctor_isDeleted")
   @@map("doctor")
+}
+*/
+
+import { Gender } from "../../../generated/prisma/enums";
+
+export interface ICreateDoctorPayload {
+	password: string;
+	doctor: {
+		name: string;
+		email: string;
+		profilePhoto?: string;
+		contactNumber?: string;
+		address?: string;
+		registrationNumber?: string;
+		experience?: number;
+		gender?: Gender;
+		appointmentFee?: number;
+		qualification?: string;
+		currentWorkingPlace?: string;
+		designation?: string;
+	};
+	specialties: string[];
 }
