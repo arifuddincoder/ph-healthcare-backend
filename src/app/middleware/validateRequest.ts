23bfcde8aa39eadaed29e1c5b1fa49936f5f4e7c
arifuddincoder <arifuddincoder@gmail.com>
2026-04-10 23:22:41 +0600

    add cloudinary to upload files

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -5,6 +5,10 @@ import z from "zod";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const validateRequest = (zodSchema: z.ZodObject<any>) => {
 	return (req: Request, res: Response, next: NextFunction) => {
+		if (req.body.data) {
+			req.body = JSON.parse(req.body.data);
+		}
+
 		const parsedResult = zodSchema.safeParse(req.body); // req.body
 
 		if (!parsedResult.success) {
